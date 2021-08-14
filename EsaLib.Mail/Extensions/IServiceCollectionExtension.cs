01826a1014c36a80dea235a45f2cb19d29024b0c
Omar SAIDI <omar.saidi@mediawan.com>
2021-08-14 10:11:50 +0200

    add service extensions

---
+++ EsaLib.Mail/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EsaLib.Mail.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection AddLibMailServices(this IServiceCollection services)
+        {
+            services.AddScoped<IEmailSender, EmailSender>();
+            services.AddScoped<ITokenizer, Tokenizer>();
+            return services;
+        }
+    }
+}
